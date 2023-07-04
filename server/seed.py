e071e7986e59ab9dbfb8ca6c5fc543c5e1e6eff7
Evan <eroberts1998@icloud.com>
2023-07-03 23:43:52 -0400

    data seeded

--- server/seed.py
+++ server/seed.py
@@ -1,10 +1,35 @@
 from app import app
-# from models import db
+from models import db, User, Payment_Detail, Address, Product, Category, Review, Cart, Cart_Item, Order, OrderItem, Order_Status
 import pickle
 import os
 import ipdb
 import json
 
 
+def view_pickle_structure(filename):
+    with open(filename, 'rb') as f:
+        data = pickle.load(f)
+    print(data)
+
+
+# def restore_categories():
+
+#     db.session.query(Category).delete()
+
+#     with open('data-backup/categories_backup.pickle', 'rb') as file:
+#         data = pickle.load(file)
+
+#     for category in data:
+#         category = Category(
+#             name=category.name
+#         )
+#         db.session.add(category)
+
+#     db.session.commit()
+
+
+
 if __name__ == "__main__":
-    pass
+    with app.app_context():
+
+        pass
