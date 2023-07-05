541c50b6c6b83b1961958f9d5493f1c89291fbd8
Evan <eroberts1998@icloud.com>
2023-07-05 16:34:56 -0400

    Nav and Footer complete, will add Link to as functionality is added

---
+++ client/src/components/Address.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Address() {
+  return (
+    <div>Address</div>
+  )
+}
+
+export default Address
---
+++ client/src/components/pages/Causes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Causes() {
+  return (
+    <div>Causes</div>
+  )
+}
+
+export default Causes
