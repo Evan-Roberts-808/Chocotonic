beb971fcc4232fff0e61056b7f6b49c6a4b7c023
Evan <eroberts1998@icloud.com>
2023-07-07 13:44:35 -0400

    components done with placeholder images and text

--- client/src/components/pages/About.jsx
+++ client/src/components/pages/About.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <>
+      <Row>
+        <div className="horizontal-line-container">
+          <div className="horizontal-line"></div>
+          <img
+            className="emblem"
+            src="https://raw.githubusercontent.com/Evan-Roberts-808/Capstone/9f264e240c57cfadce2104eccd29fd14b0422090/.github/images/site-images/illustration.svg"
+          />
+          <div className="horizontal-line"></div>
+        </div>
+      </Row>
+      <Row>
+        <div className="d-flex justify-content-center">
+          <h3>About Us</h3>
+        </div>
+      </Row>
+      <Row>
+  <div className="about-jumbotron jumbotron-fluid">
+    <Container>
+      <div className="jumbotron-content">
+        <p className="jumbotron-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id ante vel eros scelerisque tempus eget sed justo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id ante vel eros scelerisque tempus eget sed justo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id ante vel eros scelerisque tempus eget sed justo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id ante vel eros scelerisque tempus eget sed justo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id ante vel eros scelerisque tempus eget sed justo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id ante vel eros scelerisque tempus eget sed justo.
+        </p>
+      </div>
+    </Container>
+  </div>
+</Row>
+      <Container>
+        <Row>
+          <Col md={4}>
+            <div className="about-column">
+              <h4>Our Mission</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                id ante vel eros scelerisque tempus eget sed justo.
+              </p>
+            </div>
+          </Col>
+          <Col md={4}>
+            <div className="about-column">
+              <h4>Our Vision</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                id ante vel eros scelerisque tempus eget sed justo.
+              </p>
+            </div>
+          </Col>
+          <Col md={4}>
+            <div className="about-column">
+              <h4>Our Values</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                id ante vel eros scelerisque tempus eget sed justo.
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
-export default About
+export default About;
